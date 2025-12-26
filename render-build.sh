9c0f365927a2322548125370b21b0f992946264a
AnkitaSingh8349 <ankita@ajxtechnologies.com>
2025-12-26 18:04:31 +0530

    Install system libs for WeasyPrint on Render

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-set -o errexit
+set -e
+
+apt-get update
+apt-get install -y \
+  libpango-1.0-0 \
+  libcairo2 \
+  libgdk-pixbuf-2.0-0 \
+  libffi-dev
 
 pip install -r requirements.txt
+
 python manage.py migrate
 python manage.py collectstatic --noinput
